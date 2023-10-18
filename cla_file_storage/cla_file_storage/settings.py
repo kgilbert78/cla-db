2ad1c48c8222f8e87d7b0da909e249692bbc1202
kgilbert78 <kgilbert78@gmail.com>
2023-10-18 19:03:38 -0400

    migrated db to postgreSQL

--- cla_file_storage/cla_file_storage/settings.py
+++ cla_file_storage/cla_file_storage/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'cla_file_storage.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cla_file_db",
+        "USER": "admin",
+        "PASSWORD": config("DB_PWD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
